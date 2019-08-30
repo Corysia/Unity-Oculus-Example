30c31776d32ca76f79b62f4473925af34bf201b3
Corysia Taware <corysia.taware@gmail.com>
2019-08-30 12:10:21 -0700

    trying out a debug script

---
+++ Assets/Scripts/DebugUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DebugUI : MonoBehaviour
+{
+    public static DebugUI Instance;
+    private bool inMenu = false;
+    private Text logText;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rectTransform = DebugUIBuilder.instance.AddLabel("Debug");
+        logText = rectTransform.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Two) || OVRInput.GetDown(OVRInput.Button.Start))
+        {
+            if (inMenu) DebugUIBuilder.instance.Hide();
+            else DebugUIBuilder.instance.Show();
+            inMenu = !inMenu;
+        }
+    }
+
+    public void Log(string msg)
+    {
+        logText.text = msg;
+    }
+}
